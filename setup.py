bb8834f1d027c23f3f16b5718fded30381590aa4
chandni-tache <chandni@tache.in>
2018-06-30 13:02:42 +0530

    added loader

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ build_exe_options = {
    'packages': ['PySide.QtXml','pyHook',"PySide"]
    }
 if 'bdist_msi' in sys.argv:
-	sys.argv+=["--initial-target-dir",'']
+	sys.argv+=["--initial-target-dir",r'%appdata%\\provolog']
 
 setup(name='ProvLock',
      version = '0.10',
